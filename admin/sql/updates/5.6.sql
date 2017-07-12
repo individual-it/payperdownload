d892999ddb6e8779395cb842a0ceec6723dcd9e2
Artur Neumann <artur@jankaritech.com>
2017-07-12 09:43:08 +0545

    fix SQL update

--- admin/sql/updates/5.6.sql
+++ admin/sql/updates/5.6.sql
@@ -1,4 +1,4 @@
 ALTER TABLE #__payperdownloadplus_resource_licenses
-	CHANGE `resource_price` VARCHAR(255) NOT NULL;
+	CHANGE `resource_price` `resource_price` VARCHAR(255) NOT NULL;
 ALTER TABLE #__payperdownloadplus_resource_licenses
-	CHANGE `download_expiration` VARCHAR(255) DEFAULT 365;
+	CHANGE `download_expiration` `download_expiration` VARCHAR(255) DEFAULT 365;
